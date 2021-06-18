4ff195d027e9bf1ebbff3dbb3f2a96aa0aa1190b
NorthernAnvil <michael.g@northernanvil.com>
2021-06-19 00:31:13 +0200

    ger upp!

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-myTitle = document.getElementById('anim');
-console.log(myTitle);
+let mouseCursor = document.querySelector(".cursor");
+
+window.addEventListener('mousemove', cursor);
+
+function cursor(e) {
+    mouseCursor.style.top = e.pageY + 'px';
+    mouseCursor.style.left = e.pageX + 'px';
+}
